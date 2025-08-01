3f7285408cca69e3a5368f2d6c286bae715ba61a
Matheus Oberziner <matheusschmidtoberziner@gmail.com>
2025-08-01 17:27:10 -0300

    Adicionado o componente

---
+++ src/components/LevelSteps/StepLevel4.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="col-12 row q-col-gutter-md" style="padding-top: 100px;">
+    <!-- <div
+      v-for="index in 2"
+      :key="index"
+      class="col-6 row justify-center"
+    >
+      <CardsComponent type="type5"/>
+    </div> -->
+  </div>
+</template>
+<script>
+// import CardsComponent from '../CardsComponent.vue'
+export default {
+  components: {
+    // CardsComponent
+  }
+}
+</script>
